3aa8a27b0903240cc61304028eaf05e1580e2fed
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-11-21 14:17:45 +0100

    Det jeg fik nået af opgaven.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Navigate, useLoaderData } from "react-router";
 
+
 export default function Home () {
 
   const pets = useLoaderData();
@@ -16,9 +17,22 @@ export default function Home () {
   return (
     <>
       
+    
+
       <h1>Home</h1>
 
+      <img src="/filebucket/user.jpg" alt="User" className="icon" width="40px" height="40px" ></img>
+
+      
+      
+  
       {pets.map(pet => <img key={pet.id} src={pet.image} alt={pet.breed} />)}
+
+      
+
+      
+
+
     </>
           
   )
